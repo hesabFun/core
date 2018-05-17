3b9ef1848a847da6f09f5404fd743d19e5c02613
ErFUN KH <erfun.kh@gmail.com>
2018-05-17 11:51:49 +0430

    added: tests

---
+++ categories.controller_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestInsertNewCategory(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+
+	var category Categorise
+	category.Name = "test category"
+	category.Order = 1
+	category.ParentId = 0
+	jsonValue, _ := json.Marshal(category)
+
+	req, _ := http.NewRequest("POST", "/v1/companies/1/categories", bytes.NewBuffer(jsonValue))
+	req.Header.Add("Authorization", "Bearer "+os.Getenv("JWT_TEST_TOKEN"))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+	//todo: test json schema
+}
---
+++ products.controller_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestInsertNewProduct(t *testing.T) {
+	router := setupRouter()
+	w := httptest.NewRecorder()
+
+	var product Products
+	product.Name = "test category"
+	product.CategoryId = 1
+	product.Description = "product description"
+	product.Price = 10000
+	jsonValue, _ := json.Marshal(product)
+
+	req, _ := http.NewRequest("POST", "/v1/companies/1/products", bytes.NewBuffer(jsonValue))
+	req.Header.Add("Authorization", "Bearer "+os.Getenv("JWT_TEST_TOKEN"))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+	//todo: test json schema
+}
