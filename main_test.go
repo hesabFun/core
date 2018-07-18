6eeb48b23aa95a2f70b6cd84796430a7645f1133
ErFUN KH <erfun.kh@gmail.com>
2018-07-18 18:31:21 +0430

    change test

--- main_test.go
+++ main_test.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	_ "github.com/joho/godotenv/autoload"
-	"github.com/stretchr/testify/assert"
-
 	"encoding/json"
 	"fmt"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 	"time"
 )
 
 func TestMainExecution(t *testing.T) {
+	c := make(chan int, 1)
+	c <- 0
+	os.Setenv("PORT", "7060")
+
 	go main()
 	time.Sleep(time.Second)
-	resp, err := http.Get("http://localhost:8080/")
+	resp, err := http.Get("http://localhost:7060/")
 	if err != nil {
 		t.Fatalf("Cannot make get: %v\n", err)
 	}
@@ -32,4 +36,6 @@ func TestMainExecution(t *testing.T) {
 	}
 	assert.Equal(t, map[string]interface{}{"message": "Hello world!"}, decodedResponse,
 		"Should return status:ok")
+
+	close(c)
 }
