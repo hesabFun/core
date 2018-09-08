4660f0698621370546a1d796e5bcda88df8cc957
ErFUN KH <erfun.kh@gmail.com>
2018-09-08 15:55:14 +0430

    change docs

--- apidoc/doc.go
+++ apidoc/doc.go
@@ -3,6 +3,7 @@ package apidoc
 /**
  * @apiDefine jwt
  * @apiHeader {String} Authorization Authorization value.
+ * @apiSampleRequest off
  * @apiHeaderExample {String} Header-Example:
  *		Authorization: Bearer xxx.xxx.xxx
  */
